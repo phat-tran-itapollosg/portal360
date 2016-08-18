5a31a702fa0ea3effd68212507b20a79a4c5f915
tanphatbitbucket <excellen.it@gmail.com>
2016-08-18 14:48:17 +0700

    Save ID on iduser alphal_faq

---
+++ app/views/news/index.blade.php
@@ -0,0 +1,18 @@
+<!--
+ *
+ *Alpha team Tran Tan Phat
+ *trantanphat.it@gmail.com
+ *
+ */
+-->
+@extends('layouts.master')
+@section('content')
+<link rel="stylesheet" href="{{ URL::asset('public/css/css.css' )}}">
+<div class='content'>
+
+    <h1>
+        hi ku news
+    </h1>
+    
+</div>
+@stop
