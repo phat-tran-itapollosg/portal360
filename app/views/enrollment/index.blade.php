5c45e966488565ccc74954d960be10c24d105da2
locpt <locpt@younetco.com>
2016-08-21 20:39:53 +0700

    [UI] profile page, enrollment page

--- app/views/enrollment/index.blade.php
+++ app/views/enrollment/index.blade.php
@@ -3,10 +3,186 @@
 @section('title', trans('enrollment_index.page_title'))
 
 @section('styles')
-   
 @stop
 
 @section('content')
+<!-- page start-->
+      <div class="row">
+      
+        <div class="col-lg-4">
+                @foreach($enrollents as $key => $enrollment)
+                @if ($key % 3 === 0)
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="bio-chart">
+                            <div style="display:inline;width:101px;height:101px;">
+                                </canvas>                          
+                                <img src="{{ URL::asset('/public/img/logo_apollo.png') }}" alt="Trung Tâm Anh Ngữ Quốc Tế Apollo" width="101" height="101px"/>      
+                            </div>
+                        </div>
+                        <div class="bio-desk" style="color:#aec785">
+                            <h4 class="terques">{{ $enrollment->class_name }}</h4>
+                            <p><b>{{trans('enrollment_index.ec')}}: </b>{{ $enrollment->ec_name }}</p>
+                            <p><b>{{trans('enrollment_index.center')}}: </b>{{ $enrollment->center }}</p>
+                        </div>
+                        <div class="bio-content">
+                            <table style="float:left;margin-left:5px">
+                              <col width="130">
+                              <col width="80">
+                              <tr>
+                                <td><b>{{trans('enrollment_index.total_amount')}}:</b></td>
+                                <td>{{ $enrollment->total_amount }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.ending_balance')}}: </b></td>
+                                <td>{{ $enrollment->balance }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.total_hour')}}:</b></td>
+                                <td>{{ $enrollment->total_hour }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.ending_hour')}}:</b></td>
+                                <td>{{ $enrollment->balance_hour }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.start_date')}}:</b></td>
+                                <td>{{ SugarUtil::formatDate($enrollment->start_date) }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.end_date')}}:</b></td>
+                                <td>{{ SugarUtil::formatDate($enrollment->end_date) }}</td>
+                              </tr>
+                            </table>                            
+                        </div>
+                    </div>
+                </div>
+                @endif
+                @endforeach
+        </div>
+        <div class="col-lg-4">
+                @foreach($enrollents as $key => $enrollment)
+                @if ($key % 3 === 1)
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="bio-chart">
+                            <div style="display:inline;width:101px;height:101px;">
+                                </canvas>                          
+                                <img src="{{ URL::asset('/public/img/logo_apollo.png') }}" alt="Trung Tâm Anh Ngữ Quốc Tế Apollo" width="101" height="101px"/>      
+                            </div>
+                        </div>
+                        <div class="bio-desk" style="color:#aec785">
+                            <h4 class="terques">{{ $enrollment->class_name }}</h4>
+                            <p><b>{{trans('enrollment_index.ec')}}: </b>{{ $enrollment->ec_name }}</p>
+                            <p><b>{{trans('enrollment_index.center')}}: </b>{{ $enrollment->center }}</p>
+                        </div>
+                        <div class="bio-content">
+                            <table style="float:left;margin-left:5px">
+                              <col width="130">
+                              <col width="80">
+                              <tr>
+                                <td><b>{{trans('enrollment_index.total_amount')}}:</b></td>
+                                <td>{{ $enrollment->total_amount }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.ending_balance')}}: </b></td>
+                                <td>{{ $enrollment->balance }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.total_hour')}}:</b></td>
+                                <td>{{ $enrollment->total_hour }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.ending_hour')}}:</b></td>
+                                <td>{{ $enrollment->balance_hour }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.start_date')}}:</b></td>
+                                <td>{{ SugarUtil::formatDate($enrollment->start_date) }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.end_date')}}:</b></td>
+                                <td>{{ SugarUtil::formatDate($enrollment->end_date) }}</td>
+                              </tr>
+                            </table>                            
+                        </div>
+                    </div>
+                </div>
+                @endif
+                @endforeach
+        </div>
+        <div class="col-lg-4">
+                @foreach($enrollents as $key => $enrollment)
+                @if ($key % 3 === 2)
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="bio-chart">
+                            <div style="display:inline;width:101px;height:101px;">
+                                </canvas>                          
+                                <img src="{{ URL::asset('/public/img/logo_apollo.png') }}" alt="Trung Tâm Anh Ngữ Quốc Tế Apollo" width="101" height="101px"/>      
+                            </div>
+                        </div>
+                        <div class="bio-desk" style="color:#aec785">
+                            <h4 class="terques">{{ $enrollment->class_name }}</h4>
+                            <p><b>{{trans('enrollment_index.ec')}}: </b>{{ $enrollment->ec_name }}</p>
+                            <p><b>{{trans('enrollment_index.center')}}: </b>{{ $enrollment->center }}</p>
+                        </div>
+                        <div class="bio-content">
+                            <table style="float:left;margin-left:5px">
+                              <col width="130">
+                              <col width="80">
+                              <tr>
+                                <td><b>{{trans('enrollment_index.total_amount')}}:</b></td>
+                                <td>{{ $enrollment->total_amount }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.ending_balance')}}: </b></td>
+                                <td>{{ $enrollment->balance }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.total_hour')}}:</b></td>
+                                <td>{{ $enrollment->total_hour }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.ending_hour')}}:</b></td>
+                                <td>{{ $enrollment->balance_hour }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.start_date')}}:</b></td>
+                                <td>{{ SugarUtil::formatDate($enrollment->start_date) }}</td>
+                              </tr>
+                              <tr>
+                                <td><b>{{trans('enrollment_index.end_date')}}:</b></td>
+                                <td>{{ SugarUtil::formatDate($enrollment->end_date) }}</td>
+                              </tr>
+                            </table>                            
+                        </div>
+                    </div>
+                </div>
+                @endif
+                @endforeach
+        </div>
+
+                <!--
+                <tr>
+                    <td>{{ $key+1 }}</td>
+                    <td>{{ $enrollment->class_name }} </td>
+                    <td>{{ SugarUtil::formatDate($enrollment->start_date) }} </td>
+                    <td>{{ SugarUtil::formatDate($enrollment->end_date) }} </td>
+                    <td>{{ $enrollment->total_amount }} </td>
+                    <td>{{ $enrollment->balance }} </td>
+                    <td>{{ $enrollment->total_hour }} </td>
+                    <td>{{ $enrollment->balance_hour }} </td>
+                    <td>{{ $enrollment->ec_name }} </td>
+                    <td>{{ $enrollment->center }} </td>       
+                </tr>
+                -->
+                
+
+      </div>
+
+  <!-- page end-->
+<!--
     <div class="container">
         <div class="block-header">
             <h2>{{ trans('enrollment_index.page_title') }}</h2>
@@ -50,11 +226,14 @@
         </div>
     </div>
     
-	
+-->
 @stop
 
 @section('scripts')
+<!--
     <script src="{{ URL::asset('public/vendors/datatables/media/js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ URL::asset('public/vendors/datatables/media/js/dataTables.bootstrap.min.js') }}"></script>
     <script src="{{ URL::asset('public/js/datatabel.js') }}"></script>
+-->
+    <script src="{{ URL::asset('public/theme/assets/jquery-knob/js/jquery.knob.js') }}"></script>
 @stop
--- app/views/user/profile.blade.php
+++ app/views/user/profile.blade.php
@@ -7,108 +7,98 @@
 @stop
 
 @section('content')
-    
-    <div class="container">
-        <div class="block-header">
-            <h2>{{ trans('user_profile.page_title') }}</h2>
-        </div>
-        
-        <div class="card">
-            <div class="card-body card-padding">
-                <form id="form-profile" action="{{ URL::to('user/saveProfile') }}" method="post">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label class="control-label">{{ trans('user_profile.customer_id') }}</label>
-                                <div class="fg-line">
-                                    <input type="text" id="customer_id" name="customer_id" class="form-control" placeholder="{{ trans('user_profile.customer_id') }}" value="{{ $user->user_name }}" disabled>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                
-                                <div class="fg-line">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label class="control-label">{{ trans('user_profile.last_name') }}</label>
-                                <div class="fg-line">
-                                    <input type="text" id="last_name" name="last_name" class="form-control" placeholder="{{ trans('user_profile.last_name') }}" value="{{ $user->last_name }}">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label class="control-label">{{ trans('user_profile.first_name') }}</label>
-                                <div class="fg-line">
-                                    <input type="text" id="first_name" name="first_name"  class="form-control" placeholder="{{ trans('user_profile.first_name') }}" value="{{ $user->first_name }}">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                
-                                <div class="fg-line">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label class="control-label">{{ trans('user_profile.mobile_phone') }}</label>
-                                <div class="fg-line">
-                                    <input type="text" id="mobile_phone" name="mobile_phone"  class="form-control" placeholder="{{ trans('user_profile.mobile_phone') }}" value="{{ $user->phone_mobile }}">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label class="control-label">{{ trans('user_profile.email') }}</label>
-                                <div class="fg-line">
-                                    <input type="text" id="email" name="email" class="form-control" placeholder="{{ trans('user_profile.email') }}" value="{{ $user->email1 }}">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label class="control-label">{{ trans('user_profile.address') }}</label>
-                                <div class="fg-line">
-                                    <input type="text" id="address" name="address"  class="form-control" placeholder="{{ trans('user_profile.address') }}" value="{{ $user->address_street }}">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="form-group">
-                                <label class="control-label">{{ trans('user_profile.description') }}</label>
-                                <div class="fg-line">
-                                    <textarea type="text" id="description" name="description" rows="5" class="form-control" placeholder="{{ trans('user_profile.description') }}">{{ $user->description }}</textarea>    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <button type="submit" class="btn btn-primary btn-sm m-t-10">{{ trans('app.btn_save_text') }}</button>
-                </form>
-            </div>
-        </div>
-    </div>
+<!-- page start-->
+  <div class="row">
+      <aside class="profile-nav col-lg-3">
+          <section class="panel">
+              <div class="user-heading round">
+                  <a href="#">
+                        @if(Session::get('contact')->picture != '')
+                        <img src="{{ Config::get('app.service_config.server_url') }}/index.php?entryPoint=getAvatar&id={{Session::get('contact')->picture}}&type=SugarFieldImage&isTempFile=1" alt="">
+                         @else
+                        <img src="{{ URL::asset('public/img/customer-avatar.png') }}" alt="">
+                         @endif
+                  </a>
+                  <h1>{{ Session::get('contact')->last_name }} {{ Session::get('contact')->first_name }}</h1>
+                  <p>{{ Session::get('user')->user_name }}</p>
+              </div>
+<!--
+              <ul class="nav nav-pills nav-stacked">
+                  <li class="active"><a href="{{ URL::to('user/profile') }}"> <i class="fa fa-user"></i> {{ trans('app.view_profile') }}</a></li>
+              </ul>
+              -->
+              <ul class="nav nav-pills nav-stacked">
+                <li>
+                    <a><b><i class="fa fa-map-marker"></i> {{ trans('user_profile.address') }}:</b> {{ $user->address_street }}</a>
+                </li>     
+                <li>
+                    <a><b><i class="fa fa-envelope"></i> {{ trans('user_profile.email') }}:</b> {{ $user->email1 }}</a>
+                </li>     
+                <li>
+                    <a><b><i class="fa fa-mobile-phone"></i> {{ trans('user_profile.mobile_phone') }}:</b> {{ $user->phone_mobile }}</a>
+                </li>
+              </ul>
+                                                        
+          </section>
+            <aside>
+                                       
+           
+            </aside>
+      </aside>
+      <aside class="profile-info col-lg-9">
+          <section class="panel">
+              <div class="panel-body bio-graph-info">
+                  <h1>Edit Profile</h1>
+                  <form id="form-profile" action="{{ URL::to('user/saveProfile') }}" method="post" class="form-horizontal" role="form">
+                      <div class="form-group">
+                          <label  class="col-lg-2 control-label"><b>{{ trans('user_profile.customer_id') }}</b></label>
+                          <div class="col-lg-10">
+                              <input type="text" id="customer_id" name="customer_id" class="form-control" placeholder="{{ trans('user_profile.customer_id') }}" value="{{ $user->user_name }}" disabled>
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label  class="col-lg-2 control-label"><b>{{ trans('user_profile.first_name') }}</b></label>
+                          <div class="col-lg-6">
+                              <input type="text" id="last_name" name="last_name" class="form-control" placeholder="{{ trans('user_profile.first_name') }}" value="{{ $user->first_name }}">
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label  class="col-lg-2 control-label"><b>{{ trans('user_profile.last_name') }}</b></label>
+                          <div class="col-lg-6">
+                              <input type="text" id="last_name" name="last_name" class="form-control" placeholder="{{ trans('user_profile.last_name') }}" value="{{ $user->last_name }}">
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label  class="col-lg-2 control-label"><b>{{ trans('user_profile.address') }}</b></label>
+                          <div class="col-lg-6">
+                               <input type="text" id="address" name="address"  class="form-control" placeholder="{{ trans('user_profile.address') }}" value="{{ $user->address_street }}">
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label  class="col-lg-2 control-label"><b>{{ trans('user_profile.email') }}</b></label>
+                          <div class="col-lg-6">
+                              <input type="text" id="email" name="email" class="form-control" placeholder="{{ trans('user_profile.email') }}" value="{{ $user->email1 }}">
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label  class="col-lg-2 control-label"><b>{{ trans('user_profile.mobile_phone') }}</b></label>
+                          <div class="col-lg-6">
+                              <input type="text" id="mobile_phone" name="mobile_phone"  class="form-control" placeholder="{{ trans('user_profile.mobile_phone') }}" value="{{ $user->phone_mobile }}">
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <div class="col-lg-offset-2 col-lg-10">
+                              <button type="submit" class="btn btn-primary btn-sm m-t-10">{{ trans('app.btn_save_text') }}</button>
+                          </div>
+                      </div>
+                  </form>
+                  
+              </div>
+          </section>                      
+      </aside>
+  </div>
 
+  <!-- page end-->
 @stop
 
 @section('scripts')
@@ -117,6 +107,7 @@
     
     <script src="{{ URL::asset('public/vendors/jquery-validate/jquery.validate.min.js') }}"></script>
     <script src="{{ URL::asset('public/js/user_profile.js') }}"></script>
+    <script src="{{ URL::asset('public/theme/assets/jquery-knob/js/jquery.knob.js') }}"></script>
 
     @if(Session::has('flash_message'))
         <script type="text/javascript">
