707cf3019201ce5fc6faf062aaf11da0228657a1
mr.tanphat <excellen.it@gmail.com>
2016-08-30 00:59:17 +0700

    add views packages

---
+++ app/views/packages/index.blade.php
@@ -0,0 +1,42 @@
+<!--
+ *
+ *Alpha team Tran Tan Phat
+ *trantanphat.it@gmail.com
+ *
+ */
+-->
+<link href="{{ URL::asset('public/theme/css/tasks.css') }}" rel="stylesheet">  
+
+@extends('layouts.master')
+@section('content')
+  <div class='row'>
+    <div class="col-lg-12">  
+      <section class="panel panel-default">
+        <header class="panel-heading" style="border-radius:0; text-align: center; ">
+          <b> Alphal Admin </b>
+        </header>
+        <div class="panel-body">
+                <ul class='ulli'>
+                
+
+                    <li class="boxfaq" style="padding:10px">
+
+                        <div class="task-title imgquestion" style="display:inline-block"> 
+                            
+                        </div>
+                        <div class="questionhv" style="display:inline-block; padding-left:10px">
+
+                            
+                            <br>    
+                            Danh mục:    
+                            
+                        </div>
+                        <div style="float: right; ">
+                        </div>
+                    </li>
+                </ul>
+        </div>
+      </section>
+    </div>
+  </div>
+@stop
