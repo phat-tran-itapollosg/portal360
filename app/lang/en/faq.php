4691e3612fff8bbc57883c6de0bf3a63eb91c84a
Pong Pi <eroshaly@gmail.com>
2016-09-09 09:28:40 +0700

    Add Lang FAQ

---
+++ app/lang/en/faq.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'faqquestion' => 'Question',
+	'faqreply' => 'Reply',
+	'faqdate' => 'Created At',
+	'img' => 'Image',
+	'edit' => 'Edit',
+	'delete' => 'Delete',
+	'updateImage' => 'Update Image',
+	'heading' => 'FAQ',
+	);
+?>
---
+++ app/lang/vi/faq.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'faqquestion' => 'Câu hỏi',
+	'faqreply' => 'Trả lời',
+	'faqdate' => 'Thời gian',
+	'img' => 'Hình ảnh',
+	'edit' => 'Cập Nhật',
+	'delete' => 'Xóa',
+	'updateImage' => 'Cập nhật hình ảnh',
+	'heading' => 'Câu hỏi thường gặp',
+	);
+?>
