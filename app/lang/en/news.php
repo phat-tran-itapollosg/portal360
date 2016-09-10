ed931021379e9211295b23ba0f4c9385c1b28724
mr.tanphat <excellen.it@gmail.com>
2016-09-10 11:42:05 +0700

    add news lang news

---
+++ app/lang/en/news.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+	'heading' => 'News',
+	'title'	=> ' Title ',
+	'img'	=>'Image',
+	'date'	=>'Date',
+	'edit'	=> 'Update',
+	'add'	=> 'Add new',
+	'delete'=> 'Delete',
+	'updateImage' => 'updateImage',
+	'submit'	=>'Update',
+	'back'	=> 'Back',
+	'contents'	=> 'Content',
+	'category'	=> 'Category',
+	);
+?>
---
+++ app/lang/vi/news.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+	'heading' => ' Tin tức ',
+	'title'	=>' Tiêu đề ',
+	'img'	=> 'Hình ảnh',
+	'date'	=> 'Ngày',
+	'edit'	=> ' Cập nhật',
+	'add'	=> 'Thêm',
+	'delete'=> 'Xóa',
+	'updateImage' => ' Cập nhật hình đại diện',
+	'submit'	=>' Lưu lại ',
+	'back'	=> ' Quay trở lại ',
+	'contents'	=> 'Nội dung',
+	'category'	=> 'Danh mục',
+	);
+?>
