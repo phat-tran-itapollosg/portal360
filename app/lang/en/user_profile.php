37f21fc82bca4a0e02c57beecbe35de0a5709d89
mr.tanphat <excellen.it@gmail.com>
2016-09-27 21:30:38 +0700

    update lang en user_profile

--- app/lang/en/user_profile.php
+++ app/lang/en/user_profile.php
@@ -5,4 +5,9 @@ return array(
     'page_title' => 'Profile',
     'customer_id' => 'Customer ID',
     'update_profile_success_msg' => 'Customer ID',
+    'first_name' => 'First Name',
+    'last_name'	=>'Last Name',
+    'address'	=>'Address',
+    'email'		=>'Email',
+    'mobile_phone'=>'Phone Number',
 );
