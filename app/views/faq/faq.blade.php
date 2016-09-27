b9c07fa75ada5b0b72b19d716889713db63cd765
mr.tanphat <excellen.it@gmail.com>
2016-09-27 20:44:46 +0700

    update Res user FAQ

--- app/views/faq/faq.blade.php
+++ app/views/faq/faq.blade.php
@@ -15,44 +15,34 @@
 <!--<link rel="stylesheet" href="{{ URL::asset('public/css/css.css' )}}"> -->
 <div class='row'>
     <div class="col-lg-12">       
-                <section class="panel panel-default">
-                <header class="panel-heading" style="border-radius:0">
-                  <b> {{ trans('faq.heading') }} </b>
-                </header>
-
-                <div class="panel-body">
-                    <ul class='ulli'>
-                <div class="faqli">
-                @foreach ($getfaq1 as $getfaq1s ) 
-
-                    <li class="boxfaq" style="padding:10px">
-
-                        <div class="task-title imgquestion" style="display:inline-block"> 
-                                
-                                @if(!empty($getfaq1s->img)) 
-                                    <img width="64px"  src="{{URL::asset('public/images/')}}/{{$getfaq1s->img}}">
-                                @else
-                                    <img width="64px"  src="{{ URL::asset('public/img/favicon_apollo.png') }}">
-                                @endif 
-                        </div>
-                        <div class="questionhv" style="display:inline-block; padding-left:10px">
-
-                            <a class='afaq' href="faq/detal/{{$getfaq1s->id}}">{{ $getfaq1s->faqquestion }}</a>
-                            <br>    
-                            {{ trans('faq.category') }}   
-                            <a class='afaq' href="faq/category/{{$getfaq1s->idcate}}">
-
-                                    {{$getfaq1s->ccontent}}
-                            </a> 
-                        </div>
-                    
-                    </li>
-                    @endforeach
-                    </div>   
-                    </ul>
-                </div>
-                </section>
-           
+        <section class="panel panel-default">
+        <header class="panel-heading" style="border-radius:0">
+          <b> {{ trans('faq.heading') }} </b>
+        </header>
+        <div class="panel-body">
+        @foreach ($getfaq1 as $getfaq1s ) 
+            <div class="media">
+              <div class="media-left">
+                <a href="faq/detal/{{$getfaq1s->id}}">
+                    @if(!empty($getfaq1s->img)) 
+                        <img width="64px"  src="{{URL::asset('public/images/')}}/{{$getfaq1s->img}}">
+                    @else
+                        <img width="64px"  src="{{ URL::asset('public/img/favicon_apollo.png') }}">
+                    @endif 
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">
+                    <a href="faq/detal/{{$getfaq1s->id}}">
+                        {{ $getfaq1s->faqquestion }}
+                    </a>
+                </h4>
+                    <span class="label label-default">{{$getfaq1s->ccontent}}</span>
+              </div>
+            </div>
+            @endforeach
+        </div>
+        </section>         
     </div>        
 </div>
 @stop
