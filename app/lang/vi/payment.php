6e8101ee35c8aed4e957437ae6e8d64a181fcf75
mr.tanphat <excellen.it@gmail.com>
2016-09-29 21:15:49 +0700

    lang mobi payment

--- app/lang/vi/payment.php
+++ app/lang/vi/payment.php
@@ -5,10 +5,13 @@ return array(
     'payment_type' => 'Loại Thanh Toán',
     'total_amount' => 'Tổng Số Tiền',
     'payment_amount' => 'Số Tiền Học Viên Đóng',
+    'payment_amount_mobi'=> 'Học Viên Đóng',
     'freebalance_amount' => 'Số Tiền Lấy Từ Thanh Toán Khác',
+    'freebalance_amount_mobi' => 'Lấy Từ Thanh Toán Khác';
     'used_amount' => 'Số Tiền Đã Sử Dụng',
     'start_date' => 'Ngày Bắt Đầu Tính Tiền',
     'payment_expired_date' => 'ngày Hết Hạn',
+    'payment_expired_date_mobi' => 'Hết Hạn',
     'total_days' => 'Số Ngày Được Sử Dụng',
     'remain_days' => 'Số Ngày Còn Lại',
     'course_fee_name' => 'tên Gói Học',
