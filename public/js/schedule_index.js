f4ccb9a7d0bf8ba59e8ccf6d33a3917c7a093a87
Pong Pi <eroshaly@gmail.com>
2016-10-02 03:43:47 +0700

    Add monthView,listMonthView

--- public/js/schedule_index.js
+++ public/js/schedule_index.js
@@ -1,62 +1,120 @@
 $(function() {
-    var calendar = $('#calendar');
+    
+    function monthView() {
+        var calendar = $('#calendar');
+        //Generate the Calendar
+        calendar.fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                // right: 'listDay,listWeek,month'
+                right: 'month,basicWeek,basicDay'
+            },
+            defaultView: 'month',
 
-    //Generate the Calendar
-    calendar.fullCalendar({
-        header: {
-//  1e1413e10f011dfebcc6b900cffce8e8da2906d0
-            left: 'prev,next today',
-            center: 'title',
-            // right: 'listDay,listWeek,month'
-            right: 'month,basicWeek,basicDay'
-        },
-        defaultView: 'month',
+            lang: locale,
+            theme: false, //Do not remove this as it ruin the design
+            selectable: true,
+            selectHelper: true,
+            editable: false,
+            navLinks: true,
+            views: {
+                listDay: { buttonText: 'list day' },
+                listWeek: { buttonText: 'list week' }
+            },
 
-        lang: locale,
-        theme: false, //Do not remove this as it ruin the design
-//  [SVN] r6072 | trung | 2016-08-12 09:21:28 +0700 (T6, 12 Th08 2016) |
-        selectable: true,
-        selectHelper: true,
-        editable: false,
-        navLinks: true,
-        views: {
-            listDay: { buttonText: 'list day' },
-            listWeek: { buttonText: 'list week' }
-        },
+            //Add Events
+            events: scheduleEvents,
+            timeFormat: "H:mm",
+            //On Day Select
+            select: function(start, end, allDay) {
+                // Do nothing
+            },  
 
-        //Add Events
-        events: scheduleEvents,
-        timeFormat: "H:mm",
-        //On Day Select
-        select: function(start, end, allDay) {
-            // Do nothing
-        },  
+            eventClick: function(calEvent, jsEvent, view) {
+                $(".session_detail .class_name").text(calEvent.class_name);
+                $(".session_detail .duration").text(calEvent.duration);
+                $(".session_detail .starttime").text(calEvent.starttime);
+                $(".session_detail .endtime").text(calEvent.endtime);
+                $(".session_detail .teacher_name").text(calEvent.teacher_name);
+               // $(".session_detail .room_name").text(calEvent.room_name);
+                
+                $('#session_detail').modal("show");                 
+            }
+        });
+        // Add Action button with dropdown in Calendar header.
+        var actionMenu =  $('#action-menu');
+        calendar.find('.fc-toolbar').append(actionMenu);
 
-        eventClick: function(calEvent, jsEvent, view) {
-            $(".session_detail .class_name").text(calEvent.class_name);
-            $(".session_detail .duration").text(calEvent.duration);
-            $(".session_detail .starttime").text(calEvent.starttime);
-            $(".session_detail .endtime").text(calEvent.endtime);
-            $(".session_detail .teacher_name").text(calEvent.teacher_name);
-           // $(".session_detail .room_name").text(calEvent.room_name);
-            
-            $('#session_detail').modal("show");                 
-        }
-    });
+        //Calendar views
+        $('body').on('click', '#fc-actions [data-view]', function(e){
+            e.preventDefault();
+            var dataView = $(this).attr('data-view');
 
-    // Add Action button with dropdown in Calendar header.
-    var actionMenu =  $('#action-menu');
-    calendar.find('.fc-toolbar').append(actionMenu);
+            $('#fc-actions li').removeClass('active');
+            $(this).parent().addClass('active');
+            calendar.fullCalendar('changeView', dataView);
+        });
+    }
 
-    //Calendar views
-    $('body').on('click', '#fc-actions [data-view]', function(e){
-        e.preventDefault();
-        var dataView = $(this).attr('data-view');
+    function listMonthView() {
+        var calendar = $('#calendar');
+        //Generate the Calendar
+        calendar.fullCalendar({
+            header: {
+                left: 'prev,next today',
+                // center: 'title',
+                // right: 'listDay,listWeek,month'
+                // right: 'month,basicWeek,basicDay'
+            },
+            defaultView: 'listMonth',
 
-        $('#fc-actions li').removeClass('active');
-        $(this).parent().addClass('active');
-        calendar.fullCalendar('changeView', dataView);
-    });
+            lang: locale,
+            theme: false, //Do not remove this as it ruin the design
+            selectable: true,
+            selectHelper: true,
+            editable: false,
+            navLinks: true,
+            // views: {
+            //     listDay: { buttonText: 'list day' },
+            //     listWeek: { buttonText: 'list week' }
+            // },
+
+            //Add Events
+            events: scheduleEvents,
+            timeFormat: "H:mm",
+            //On Day Select
+            select: function(start, end, allDay) {
+                // Do nothing
+            },  
+
+            eventClick: function(calEvent, jsEvent, view) {
+                $(".session_detail .class_name").text(calEvent.class_name);
+                $(".session_detail .duration").text(calEvent.duration);
+                $(".session_detail .starttime").text(calEvent.starttime);
+                $(".session_detail .endtime").text(calEvent.endtime);
+                $(".session_detail .teacher_name").text(calEvent.teacher_name);
+               // $(".session_detail .room_name").text(calEvent.room_name);
+                
+                $('#session_detail').modal("show");                 
+            }
+        });
+        // Add Action button with dropdown in Calendar header.
+        var actionMenu =  $('#action-menu');
+        calendar.find('.fc-toolbar').append(actionMenu);
+
+        //Calendar views
+        $('body').on('click', '#fc-actions [data-view]', function(e){
+            e.preventDefault();
+            var dataView = $(this).attr('data-view');
+
+            $('#fc-actions li').removeClass('active');
+            $(this).parent().addClass('active');
+            calendar.fullCalendar('changeView', dataView);
+        });
+    }
+
+    
     $(window).resize(function(){
         detectMobile();
     });
@@ -67,11 +125,11 @@ $(function() {
         var detect = detectNavigator();
        if(check || detect || (window.innerWidth <= 800 && window.innerHeight <= 600)) {
          
-         $('#calendar').fullCalendar( 'changeView', 'listMonth');
+         listMonthView();
          console.log('===== mobileAndTablet')
          //return true;
        } else {
-            $('#calendar').fullCalendar( 'changeView', 'month');
+        monthView();
          ///return false;
        }
     }
