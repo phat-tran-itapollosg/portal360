171cb707a7e2dbdc4985a11cd30620e87777f11b
mr.tanphat <excellen.it@gmail.com>
2016-10-29 14:40:19 +0700

    update config ckeditor

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -5,6 +5,21 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
+	 config.language = 'vi';
 	// config.uiColor = '#AADC6E';
+	// config.disableNativeSpellChecker = false;
+
+	// config.filebrowserBrowseUrl = '../ckfinder/ckfinder.html';
+	// config.filebrowserImageBrowseUrl = '../ckfinder/ckfinder.html?type=Images';
+	// config.filebrowserFlashBrowseUrl = '../ckfinder/ckfinder.html?type=Flash';
+	// config.filebrowserUploadUrl = '../ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Files';
+	// config.filebrowserImageUploadUrl = '../ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Images';
+	// config.filebrowserFlashUploadUrl = '../ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Flash';
+	
+
+	config.filebrowserBrowseUrl= 'http://localhost/test_ckfinder/';
+
+
+	// config.filebrowserBrowseUrl= 'http://localhost/test_kcfinder/';
+	
 };
