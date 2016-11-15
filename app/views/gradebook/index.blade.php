b8c31b4afb5c7b0dbbe16dcdea571c4475002d73
Pong Pi <eroshaly@gmail.com>
2016-11-15 22:20:48 +0700

    [View] Add lms-detail modal

--- app/views/gradebook/index.blade.php
+++ app/views/gradebook/index.blade.php
@@ -78,6 +78,23 @@
                 </div>
             </div>
 
+            <div class="modal fade" id="get-lms-detail" data-backdrop="static" data-keyboard="false">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header custom-modal-header">
+                            <h3 class="modal-title custom-modal-header"> </h3>                                 
+                        </div>
+
+                        <div class="modal-body overflow-auto">
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" id="btn-cancel" class="btn" data-dismiss="modal">{{trans('app.btn_close_text')}}</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 
             </div>
             </section>
