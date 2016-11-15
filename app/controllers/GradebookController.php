1c9a1a126b62a73d8533bc1c6734a3dcf97015ae
Pong Pi <eroshaly@gmail.com>
2016-11-15 22:22:17 +0700

    [Controller] Add getLMSDetail

--- app/controllers/GradebookController.php
+++ app/controllers/GradebookController.php
@@ -18,7 +18,7 @@
             $class_id = Input::get('class_id');
             $contact = Session::get('contact');
             $student_id = $contact->id;
-
+            $getLMSDetail = array();
             $datas = SugarUtil::getGradebookDetail($class_id);
             $detail_htmls = array();
             $lang = trans('gradebook_index');
@@ -175,6 +175,7 @@
                                                 <th>{$lang['Reading']}</th> 
                                                 <th>{$lang['Grammar']}</th> 
                                                 <th>{$lang['Total']}</th> 
+                                                <th></th> 
                                             </tr> 
                                         </thead> 
                                         
@@ -185,11 +186,19 @@
                                                 <td>".number_format($data->Reading,2)."</td> 
                                                 <td>".number_format($data->Grammar,2)."</td> 
                                                 <td>".number_format($data->Total,2)."</td> 
-                                                
+                                                <td>
+                                                    <td class='center'>
+                    <input name='detail' type = 'button' value = '{$lang['lbl_detail']}' class ='btn-info btn btn_lms_detail' data='{$data->sso_code}' >  
+                    </td>             
+                                                </td>
                                             </tr>
                                         
                                       </table>
                                     </div></div>";
+                                    $getLMSDetail[$data->sso_code] = $this->getLMSDetail($class_id, $student_id, $data->sso_code);
+                                    // array_push($getLMSDetail,$this->getLMSDetail($class_id, $student_id, $data->sso_code));
+                                    
+                                
                                     
                             }
                            // $detail_content .="</div>";
@@ -242,11 +251,12 @@
                 onClick=\"window.open('$url','_blank')\" >  
                 ";
             }
-
+            
             return json_encode(array(
                 'html' => $html,
                 'total_result' => $total_result,
                 'detail' => $detail_htmls,
+                'getLMSDetail' => $getLMSDetail,
             ));                
         }
 
@@ -268,15 +278,18 @@
         public function gradebookLMSportal($class_id, $student_id)
         {
             $sql_text ="
-                        SELECT result.student_id, result.lesson_name, 
+                        SELECT result.student_id, result.lesson_name, result.sso_code,
                             MAX(result.Grammar) AS Grammar, 
                             MAX(result.Reading) AS Reading,
                             MAX(result.Listening) AS Listening,
                             MAX(result.Speaking) AS Speaking,
                             (AVG(result.Grammar) + AVG(result.Reading) 
                             + AVG(result.Listening) + AVG(result.Speaking)) AS Total
-                        FROM                         
+                        FROM    
+
                         (SELECT 
+                            
+                            ac.sso_code,
                             s.sis_student_id student_id,
                             ac.name lesson_name,
                             CASE (ls.skill)
@@ -336,7 +349,145 @@
                 $results = DB::select($sql_text);
                 return $results;
             } catch (Exception $e) {
-                var_dump($e);die();
+                //var_dump($e);die();
+                return array();
+                
+            }
+            
+        }
+        public function getLMSDetail($class_id, $student_id, $sso_code) {
+            // $class_id = Input::get('class_id');
+            // $student_id = Input::get('student_id');
+            $lang = trans('gradebook_index');
+            $variable = $this->gradebookLMSportalDetail($class_id,$student_id, $sso_code);
+            $html = "<table class='table table-bordered table-striped table-hover table-condensed'>
+                <thead> 
+                
+                <tr>
+                    <th class='text-center'>{$lang['Title']}</th>
+                    <th class='text-center' colspan='4'>{$lang['LMSScore']}</th>
+                </tr>  
+                <tr> 
+                    <th></th> 
+                    <th class='text-center'>{$lang['Speaking']}</th>
+                    <th class='text-center'>{$lang['Listening']}</th> 
+                    <th class='text-center'>{$lang['Reading']}</th> 
+                    <th class='text-center'>{$lang['Grammar']}</th> 
+                   
+                </tr> 
+              
+                </thead>
+                <tbody>
+                    ";
+                    foreach ($variable as $value) {
+                       $html .= "
+                        <tr> 
+                            <td >{$value->RE_title}</td> 
+                            <td>".(floatval($value->Speaking) > 0 ? number_format($value->Speaking,2) : '')."</td> 
+                            <td>".(floatval($value->Listening) > 0 ?number_format($value->Listening,2): '')."</td> 
+                            <td>".(floatval($value->Reading) > 0 ?number_format($value->Reading,2): '')."</td> 
+                            <td>".(floatval($value->Grammar) > 0 ?number_format($value->Grammar,2): '')."</td> 
+                            
+                            
+                        </tr>";
+                    }
+
+            $html .=        "
+                </tbody>
+            </table>";//$html
+            return array(
+                'body' => $html,
+                'title' => $variable[0]->classroom_name,
+                );
+            // return json_encode(array(
+            //     'html' => $html,
+            //     // 'total_result' => $total_result,
+            //     // 'detail' => $detail_htmls,
+            // ));
+            // // echo $html;die();
+            // exit();
+        }
+        public function gradebookLMSportalDetail($class_id, $student_id, $sso_code)
+        {
+            $sql_text ="
+                        SELECT 
+                            m.ju_class_id class_id,
+                            cc.j_class_contacts_1contacts_idb student_id,
+                            c.full_student_name student_name,
+                            ac.name classroom_name,
+                            al.lesson_name RE_title,
+                            CASE (ls.skill)
+                                WHEN 'Grammar' THEN ls.score
+                                ELSE 0
+                                END 'Grammar',
+                                CASE (ls.skill)
+                                WHEN 'Reading' THEN ls.score
+                                ELSE 0
+                                END 'Reading',
+                                CASE (ls.skill)
+                                WHEN 'Listening' THEN ls.score
+                                ELSE 0
+                                END 'Listening',
+                                CASE (ls.skill)
+                                WHEN 'Speaking' THEN ls.score
+                                ELSE 0
+                            END 'Speaking'
+                        FROM
+                            alpha_lessonname al
+                        INNER JOIN
+                            alpha_classroom ac ON al.alpha_classroom_id = ac.id AND ac.sso_code = '$sso_code'
+                        INNER JOIN
+                            meetings m ON ac.sso_code = m.sso_code 
+                                        AND m.deleted = 0
+                                        AND m.ju_class_id = '$class_id' 
+                                        AND convert(date_add(m.date_start, interval -161 hour), date) <= CURDATE()
+                        INNER JOIN
+                            j_class_contacts_1_c cc ON cc.j_class_contacts_1j_class_ida = m.ju_class_id
+                                                        AND cc.deleted = 0
+                        INNER JOIN contacts c on c.id = cc.j_class_contacts_1contacts_idb
+                                                AND c.deleted = 0
+                                                AND c.id = '$student_id' 
+                        LEFT JOIN
+                            (
+                                SELECT 
+                                    MAX(alpha_lessons.score) score,
+                                    MAX(alpha_lessons.passed) passed,
+                                    alpha_lessons.skill,
+                                    alpha_lessons.level,
+                                    alpha_lessons.title,
+                                    alpha_lessons.alpha_student_id,
+                                    alpha_lessons.alpha_classroom_id,
+                                    alpha_students.sis_student_id
+                                FROM
+                                    alpha_lessons
+                                INNER JOIN alpha_students ON alpha_lessons.alpha_student_id = alpha_students.alpha_student_id
+                                WHERE
+                                    IFNULL(alpha_students.sis_student_id, '') <> ''
+                                GROUP BY alpha_student_id , alpha_classroom_id , unit_id
+                            ) 
+                            ls ON ls.alpha_classroom_id = ac.alpha_classroom_id
+                                AND ls.title = al.lesson_name  
+                                AND ls.sis_student_id = cc.j_class_contacts_1contacts_idb
+                        GROUP BY student_id , classroom_name , lesson_name
+                        ORDER BY class_id , student_id , CASE
+                                                            WHEN ac.name LIKE 'Beginner%' THEN 0
+                                                            WHEN ac.name LIKE 'Elementary%' THEN 1
+                                                            WHEN ac.name LIKE 'Pre-intermediate%' THEN 2
+                                                            WHEN ac.name LIKE 'Intermediate%' THEN 3
+                                                            WHEN ac.name LIKE 'Upper-intermediate%' THEN 4
+                                                            ELSE 5
+                                                            END ASC , ac.sis_lesson_number;
+                            ";
+
+                        /*761f2190-d0c9-85f8-bb8f-57e9ce5f299c student_id */
+                        /* e33b145e-d8b3-46cf-0649-57edc5aab064 class id*/
+            //var_dump($sql_text);
+            //die();
+            try{
+                $results = DB::select($sql_text);
+                return $results;
+            } catch (Exception $e) {
+                //var_dump($e);die();
                 return array();
                 
             }
